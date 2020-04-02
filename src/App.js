3abd0455d2e0bf528413607d344aff9dbd86a3e6
tarwin3v <tarwin.ev@gmail.com>
2020-04-02 18:11:48 +0200

    palette comp

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Palette from "./components/Palette";
+import seedColors from "./seedColors";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Palette {...seedColors[4]} />
+    </div>
+  );
+}
+
+export default App;
